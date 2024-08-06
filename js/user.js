975c2d6dc523e34fb04a05126c68f01b3fc1ecb2
Nogal1 <mcclendon.logan1234@gmail.com>
2024-08-05 20:39:30 -0700

    working error catching?

--- js/user.js
+++ js/user.js
@@ -19,12 +19,16 @@ async function login(evt) {
 
   // User.login retrieves user info from API and returns User instance
   // which we'll make the globally-available, logged-in user.
-  currentUser = await User.login(username, password);
+  try {
+    currentUser = await User.login(username, password);
 
-  $loginForm.trigger("reset");
+    $loginForm.trigger("reset");
 
-  saveUserCredentialsInLocalStorage();
-  updateUIOnUserLogin();
+    saveUserCredentialsInLocalStorage();
+    updateUIOnUserLogin();
+  } catch (error) {
+    alert("Incorrect username or password.");
+  }
 }
 
 $loginForm.on("submit", login);
@@ -41,12 +45,16 @@ async function signup(evt) {
 
   // User.signup retrieves user info from API and returns User instance
   // which we'll make the globally-available, logged-in user.
-  currentUser = await User.signup(username, password, name);
+  try {
+    currentUser = await User.signup(username, password, name);
 
-  saveUserCredentialsInLocalStorage();
-  updateUIOnUserLogin();
+    saveUserCredentialsInLocalStorage();
+    updateUIOnUserLogin();
 
-  $signupForm.trigger("reset");
+    $signupForm.trigger("reset");
+  } catch (error) {
+    alert("Username is taken");
+  }
 }
 
 $signupForm.on("submit", signup);
